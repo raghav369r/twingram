44e62aaf0a7e2dae6b1119fe23a74dad8297e732
raghav369r <raghav1010reddy@gmail.com>
2024-05-11 13:20:34 +0530

    added time indicator to posts using date-fns package

--- src/components/shared/ProfileNTime.jsx
+++ src/components/shared/ProfileNTime.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "react-avatar";
 import { useNavigate } from "react-router-dom";
+import {
+  differenceInMinutes,
+  differenceInHours,
+  differenceInDays,
+  differenceInWeeks,
+} from "date-fns";
 
 const ProfileNTime = ({ user, post }) => {
   const navigate = useNavigate();
+  const [time, setTime] = useState("");
   const handleNavigate = () => {
     if (user?._id) navigate("/user/" + user._id);
   };
+  useEffect(() => {
+    const currDate = new Date();
+    const { uploadTime } = post;
+    const minutesDiff = differenceInMinutes(currDate, uploadTime);
+    const hoursDiff = differenceInHours(currDate, uploadTime);
+    const daysDiff = differenceInDays(currDate, uploadTime);
+    const weeksDiff = differenceInWeeks(currDate, uploadTime);
+    // console.log(minutesDiff, hoursDiff, daysDiff, weeksDiff);
+    if (weeksDiff > 10) setTime(uploadTime.getDate());
+    else if (weeksDiff) setTime(weeksDiff + " weeks ago");
+    else if (daysDiff) setTime(daysDiff + " days ago");
+    else if (hoursDiff) setTime(hoursDiff + " hours ago");
+    else if (minutesDiff) setTime(minutesDiff + " weeks ago");
+    else setTime(currDate.Date());
+  }, []);
   return (
     <div className="flex gap-4 w-full items-center">
       <div className="size-10 md:size-16">
@@ -21,7 +43,7 @@ const ProfileNTime = ({ user, post }) => {
       <div>
         <h1 className="font-semibold text-md md:text-xl">{user?.name}</h1>
         <h2 className="text-gray-500 text-md md:text-lg">
-          12 hours ago . {post?.location}
+          {time} . {post?.location}
         </h2>
       </div>
     </div>
